b54c112091b509ce7dbcfefd73a04d96af979a89
amberwagoner <amberwagoner@outlook.com>
2022-12-19 11:57:59 -0800

    Task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - deletes a binary tree using recursion
+* @tree: pointer to the root node of tree to delete
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
